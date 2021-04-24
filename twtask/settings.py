703585d7f1575c19bcd3b1187b4a52bd416d577f
Arvind Shrivastava <73704515+arv011@users.noreply.github.com>
2021-04-24 23:15:50 +0530

    Update settings.py

--- twtask/settings.py
+++ twtask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9e@fi5kou@qn5q4eba--4(ak=!3f=)*r5*9ym3gq(5t9h)p2fb'
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
